6525d8f01001c32e588f8bb60fae75a0e69c817e
HTG <xorbs016@gmail.com>
2022-03-07 10:44:14 +0900

    feat : :sparkles: MySQL Connect

--- backend/back/settings.py
+++ backend/back/settings.py
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
+        'NAME': 'testdb',       # DB 이름
+        'USER': 'root',
+        'PASSWORD': 'horang22', # 설치 시 입력한 root 비밀번호 입력
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
