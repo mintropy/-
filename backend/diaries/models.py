86bf4426b1a064ce365d4827fa22f68a5220c3cd
LYJ <jun2korea@naver.com>
2022-03-08 11:22:58 +0900

    feat : :sparkles: diary models

--- backend/diaries/models.py
+++ backend/diaries/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Dairy(models.Model):
+    id = models.IntegerField(primary_key=True)
+    content = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+    date = models.DateField(auto_now=True)
+
+
+class Flower(models.Model):
+    id = models.IntegerField(primary_key=True)
+    dairies = models.ForeignKey(
+        Dairy,
+        on_delete=models.SET_NULL,
+        related_name='flower'
+    )
+    name = models.CharField()
+    sumbol = models.CharField()
+
+
+class Photo(models.Model):
+    id = models.IntegerField(primary_key=True)
+    dairies = models.ForeignKey(
+        Dairy,
+        on_delete=models.SET_NULL,
+        related_name='photos'
+    )
+    photo = models.ImageField()
