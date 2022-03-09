80285accdbf629ec2841483f5d02d75d3d2da9d2
홍태균 <xorbs016@gmail.com>
2022-03-09 18:00:58 +0900

    feat : :sparkles: oauth jwt token Oauth 토큰 받아오기

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+from django.views import View
+from django.http import JsonResponse, HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+# Create your views here.    
+@api_view(['GET'])
+@permission_classes([AllowAny, ])
+def kakaoGetLogin(request):
+    CLIENT_ID = "057aa14f717c54ff1889493df84553ed"
+    REDIRET_URL = "http://127.0.0.1:8000/accounts/kakao/login/callback/"
+    url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={0}&redirect_uri={1}".format(
+        CLIENT_ID, REDIRET_URL)
+    res = redirect(url)
+    return res
+
+@api_view(['GET'])
+@permission_classes([AllowAny, ])
+def getUserInfo(reqeust):
+    print("###########")
+    CODE = reqeust.query_params['code']
+    url = "https://kauth.kakao.com/oauth/token"
+    res = {
+            'grant_type': 'authorization_code',
+            'client_id': "057aa14f717c54ff1889493df84553ed",
+            'redirect_url': "http://127.0.0.1:8000/accounts/kakao/login/callback/",
+            'client_secret': 'none',
+            'code': CODE
+        }
+    headers = {
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+    }
+    response = requests.post(url, data=res, headers=headers)
+    # 그 이후 부분
+    tokenJson = response.json()
+    print("$#")
+    print(tokenJson)
+    userUrl = "https://kapi.kakao.com/v2/user/me" # 유저 정보 조회하는 uri
+    auth = "Bearer "+tokenJson['access_token'] ## 'Bearer '여기에서 띄어쓰기 필수!!
+    print("##")
+    print(auth)
+    HEADER = {
+        "Authorization": auth,
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8"
+    }
+    res = requests.get(userUrl, headers=HEADER)
+    return HttpResponse(res.text)
