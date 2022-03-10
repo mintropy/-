f10256ed749e9cc5454c985ebdbc08a52a2385bd
LYJ <jun2korea@naver.com>
2022-03-10 14:59:19 +0900

    feat : :sparkles: swagger for diary

--- backend/diaries/views/diary.py
+++ backend/diaries/views/diary.py
@@ -6,7 +6,10 @@ from rest_framework.viewsets import ViewSet
 
 from .schema.diary import (
     diary_list_schema,
-    diary_retrieve_schema
+    diary_create_schema,
+    diary_retrieve_schema,
+    diary_update_schema,
+    diary_delete_schema,
 )
 from ..models import Diary
 from ..serializers.diary import DiarySerializer
@@ -15,6 +18,7 @@ from ..serializers.diary import DiarySerializer
 class DiaryViewSet(ViewSet):
     model = Diary
     queryset = Diary.objects.all()
+    serializer_class = DiarySerializer
 
     @diary_list_schema
     def list(self, request):
@@ -22,6 +26,7 @@ class DiaryViewSet(ViewSet):
         serializer = DiarySerializer(diaries, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @diary_create_schema
     def create(self, request):
         serializer = DiarySerializer(data=request.data)
         if serializer.is_valid():
@@ -35,6 +40,7 @@ class DiaryViewSet(ViewSet):
         serializer = DiarySerializer(diary)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @diary_update_schema
     def update(self, request, diary_id):
         diary = get_object_or_404(Diary, id=diary_id)
         serializer = DiarySerializer(diary, data=request.data)
@@ -43,6 +49,7 @@ class DiaryViewSet(ViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @diary_delete_schema
     def destroy(self, request, diary_id):
         diary = get_object_or_404(Diary, id=diary_id)
         diary.delete()
