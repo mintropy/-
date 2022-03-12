8aab37011617447b0684c353b0f09bcd276b5669
LYJ <jun2korea@naver.com>
2022-03-12 22:05:31 +0900

    refact : :zap: refactoring photo & schemas

--- backend/diaries/views/photo.py
+++ backend/diaries/views/photo.py
@@ -17,7 +17,7 @@ class PhotoViewSet(ViewSet):
     model = Photo
     queryset = Diary.objects.all()
     serializer_class = PhotoSerializier
-    
+
     @photo_list_schema
     def list(self, request, diary_id):
         photo = Photo.objects.filter(dairies=diary_id)
@@ -27,13 +27,11 @@ class PhotoViewSet(ViewSet):
     @photo_create_schema
     def create(self, request, diary_id):
         if not Diary.objects.filter(pk=diary_id).exists():
-            return Response(
-                    status=status.HTTP_404_NOT_FOUND
-                )
-        data={
-            'id': request.data.get('id', None),
-            'photo': request.FILES.get('photo', None),
-            'diaries': diary_id,   
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        data = {
+            "id": request.data.get("id", None),
+            "photo": request.FILES.get("photo", None),
+            "diaries": diary_id,
         }
         photo = Photo.objects.filter(dairies=diary_id)
         if photo:
@@ -42,33 +40,19 @@ class PhotoViewSet(ViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     @photo_retrieve_schema
     def retrieve(self, request, diary_id):
         if not Diary.objects.filter(pk=diary_id).exists():
-            return Response(
-                    status=status.HTTP_404_NOT_FOUND
-                )
+            return Response(status=status.HTTP_404_NOT_FOUND)
         photo = Photo.objects.filter(dairies=diary_id)
         serializer = PhotoSerializier(photo)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # def update(self, request, photo_id):
-    #     photo = get_object_or_404(Photo, id=photo_id)
-    #     serializer = PhotoSerializer(photo, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     @photo_destroy_schema
     def destroy(self, request, diary_id):
         if not Diary.objects.filter(pk=diary_id).exists():
-            return Response(
-                    status=status.HTTP_404_NOT_FOUND
-                )
-        photo = Photo.objects.filter(dairies=diary_id)    
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        photo = Photo.objects.filter(dairies=diary_id)
         photo.delete()
         return Response(status=status.HTTP_200_OK)
-    
-    
