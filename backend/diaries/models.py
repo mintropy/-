faad64547e4019f723a030332f1dfe5904af5989
LYJ <jun2korea@naver.com>
2022-03-14 14:00:07 +0900

    style : :art: update photo upload path
    join에서 f-sting으로 변경

--- backend/diaries/models.py
+++ backend/diaries/models.py
@@ -1,9 +1,10 @@
+import os
 import uuid
+
 from django.db import models
 from django.utils import timezone
-import os
 
-# Create your models here.
+
 class Diary(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     content = models.CharField(
@@ -26,13 +27,8 @@ class Flower(models.Model):
 class Photo(models.Model):
     def photo_upload_path(self, instance, filename):
         date_path = timezone.now().strftime("%Y/%m/%d")
-        name = os.path.splitext(filename)[-1].lower()  # 확장자
-        return "/".join(
-            [
-                date_path,
-                name,
-            ]
-        )
+        name = os.path.splitext(filename)[-1].lower()
+        return f"{date_path}/{name}"
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     dairies = models.ForeignKey(Diary, on_delete=models.CASCADE, related_name="photos")
