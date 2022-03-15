e8d098857449144c89bb0f1efbfe72096d254dce
LYJ <jun2korea@naver.com>
2022-03-15 11:36:06 +0900

    refact : :zap: refactor accounts url, model
    사용하지 않던 accounts/view.py 삭제

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class User(AbstractUser):
     SOCIAL_CHOICES = (
-		('KA', 'Kakao'),
-        ('GO', 'Google'),
+        ("KA", "Kakao"),
+        ("GO", "Google"),
     )
-    social = models.CharField(max_length=2, choices=SOCIAL_CHOICES,default='KA')
-    social_id = models.CharField(max_length=15,null=True,blank=True)
+    social = models.CharField(max_length=2, choices=SOCIAL_CHOICES, default="KA")
+    social_id = models.CharField(max_length=15, null=True, blank=True)
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,20 +1,14 @@
-from django.urls import path, include
-# from .views2 import getUserInfo, kakaoGetLogin, kakaoUnlink
+from django.urls import path
 
 from .views.user import AccountViewSet
 
-kakaoGetLogin = AccountViewSet.as_view({'get':'kakao_login'})
-getUserInfo = AccountViewSet.as_view({'get':'kakao_user_info'})
-kakaoUnlink = AccountViewSet.as_view({'post':'kakao_unlink'})
-kakaoLoginFinish = AccountViewSet.as_view({'get':'kakao_finish'})
+
+kakaoGetLogin = AccountViewSet.as_view({"get": "kakao_login"})
+getUserInfo = AccountViewSet.as_view({"get": "kakao_user_info"})
+kakaoUnlink = AccountViewSet.as_view({"post": "kakao_unlink"})
 
 urlpatterns = [
-    # path('logins/', kakaoGetLogin),
-    # path('kakao/login/callback/', getUserInfo),
-    # path('unlink/', kakaoUnlink),
-    #
-    path('logins/', kakaoGetLogin),
-    path('kakao/login/callback/', getUserInfo),
-    path('unlink/', kakaoUnlink),
-    path('kakao/login/finish/', kakaoLoginFinish),
+    path("logins/", kakaoGetLogin),
+    path("kakao/login/callback/", getUserInfo),
+    path("unlink/", kakaoUnlink),
 ]
