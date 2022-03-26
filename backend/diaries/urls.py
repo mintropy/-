d5b7dc99a79067536bba96faaf7c3baab390b1a0
LYJ <jun2korea@naver.com>
2022-03-26 16:59:59 +0900

    feat : :sparkles: flower view & url

--- backend/diaries/urls.py
+++ backend/diaries/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views.diary import DiaryViewSet
 from .views.photo import PhotoViewSet
+from .views.flower import FlowerViewSet
 
 
 diary_list = DiaryViewSet.as_view(
@@ -31,10 +32,15 @@ photo_detail = PhotoViewSet.as_view(
     }
 )
 
+flower_list = FlowerViewSet.as_view({"get": "list"})
+flower_detail = FlowerViewSet.as_view({"get": "retrieve"})
+
 urlpatterns = [
     path("", diary_list, name="diary_list"),
     path("<int:year>/<int:month>/", diary_montly),
     path("<int:year>/<int:month>/<int:day>/", diary_daily),
     path("photo/", photo_list),
     path("photo/<str:photo_id>/", photo_detail),
+    path("flowers/", flower_list),
+    path("flowers/<int:flower_id>/", flower_detail),
 ]
