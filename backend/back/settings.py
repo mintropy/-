59ed712bf15e2376f652c41dd55d8afffe3660b3
LYJ <jun2korea@naver.com>
2022-03-30 16:04:34 +0900

    style : :art: delete unused
    settings.py 주석 부분 제거
    사용하지 않는 파일 제거

--- backend/back/settings.py
+++ backend/back/settings.py
@@ -38,9 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = ['kubernetes.docker.internal','127.0.0.1', '0.0.0.0', 'localhost']
 ALLOWED_HOSTS = ["*"]
-# ALLOWED_HOSTS = []
 
 
 # Application definition
