392187d8047766e67cb4536193a32d9a9fdefd01
LYJ <jun2korea@naver.com>
2022-04-04 11:00:12 +0900

    style : :art: formatting flower view

--- backend/diaries/views/flower.py
+++ backend/diaries/views/flower.py
@@ -7,27 +7,26 @@ from rest_framework.viewsets import ViewSet
 from ..models import Flower
 from ..serializers.flower import FlowerSerializer
 from accounts.views.user import get_kakao_user_info
-from accounts.models import User
 
 
 class FlowerViewSet(ViewSet):
     model = Flower
     queryset = Flower.objects.all()
     serializer_class = FlowerSerializer
-    
+
     def list(self, request):
         flowers = Flower.objects.all()
         serializer = FlowerSerializer(flowers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
     def retrieve(self, request, flower_id):
         flower = get_object_or_404(Flower, id=flower_id)
         serialiezr = FlowerSerializer(flower)
         return Response(serialiezr.data, status=status.HTTP_200_OK)
-    
+
     def user(self, request):
         token = request.headers.get("Authorization", "")
-        user = get_kakao_user_info(token)        
+        user = get_kakao_user_info(token)
         flowers = user.flowers.all()
         serializer = FlowerSerializer(flowers, many=True)
         return Response(serializer.data)
