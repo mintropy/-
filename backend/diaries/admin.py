48b696aa1dfcb7b90009e5582e32d3670a7526c6
LYJ <jun2korea@naver.com>
2022-04-04 15:16:39 +0900

    feat : :sparkles: update admin
    user-flower 관계에 대해서 admin 수정

--- backend/diaries/admin.py
+++ backend/diaries/admin.py
@@ -1,24 +1,14 @@
 from django.contrib import admin
-from .models import Diary, Flower, Photo
-
-
-class PhotoInline(admin.TabularInline):
-    model = Photo
-    extra = 1
+from .models import Diary, Flower
 
 
 @admin.register(Diary)
 class DiaryAdmin(admin.ModelAdmin):
     list_display = ('user', 'date', 'custom_content', 'flower',)
-    inlines = (PhotoInline,)
     ordering = ('-date',)
 
 
 @admin.register(Flower)
 class FlowerAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'symbol',)
-
-
-@admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('dairies', 'photo',)
+    filter_horizontal  = ('users',)
